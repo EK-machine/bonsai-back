a559f2dc81f684a0358c266674a3c2d85a4fe1d7
Yauhen.kalabukhin <yauhen.kalabukhin@gmail.com>
2023-10-29 16:58:36 +0300

    feat: implemented deletion of bonsai by id

--- src/bonsai/bonsai.controller.ts
+++ src/bonsai/bonsai.controller.ts
@@ -1,20 +1,27 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   NotFoundException,
   Param,
   Post,
+  UseGuards,
 } from '@nestjs/common';
+import { AtPublic } from '../common/decorators/atPublic.decorator.js';
+import { AtGuard } from '../common/guards/at.guard.js';
 import { BonsaiService } from './bonsai.service.js';
 import { BONSAI_NOT_FOUND } from './consts/bonsai.constants.js';
 import { CreateBonsaiDto } from './dto/CreateBonsai.dto.js';
 
 @Controller('bonsai')
+@UseGuards(AtGuard)
 export class BonsaiController {
   constructor(private readonly bonsaiService: BonsaiService) {}
+
+  @AtPublic()
   @Get()
   @HttpCode(HttpStatus.OK)
   async getAllBonsai() {
@@ -25,7 +32,9 @@ export class BonsaiController {
     return allBonsai;
   }
 
+  @AtPublic()
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
   async getBonsaiById(@Param('id') id: string) {
     const bonsai = await this.bonsaiService.getBonsaiById(+id);
     if (!bonsai) {
@@ -35,7 +44,14 @@ export class BonsaiController {
   }
 
   @Post('create')
+  @HttpCode(HttpStatus.CREATED)
   async createBonsai(@Body() createBonsaiDto: CreateBonsaiDto) {
     this.bonsaiService.createBonsai(createBonsaiDto);
+  }
+
+  @Delete('delete/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deleteById(@Param('id') id: string) {
+    await this.bonsaiService.deleteById(+id);
   }
 }
--- src/bonsai/bonsai.service.ts
+++ src/bonsai/bonsai.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Bonsai } from '../typeorm/entities/index.js';
 import { CreateBonsaiParams } from '../utils/types.js';
+import { BONSAI_NOT_FOUND } from './consts/bonsai.constants.js';
 
 @Injectable()
 export class BonsaiService {
@@ -21,5 +22,13 @@ export class BonsaiService {
   async createBonsai(bonsaiDetails: CreateBonsaiParams) {
     const newBonsai = this.bonsaiRepository.create({ ...bonsaiDetails });
     return this.bonsaiRepository.save(newBonsai);
+  }
+
+  async deleteById(id: number) {
+    const bonsaiToDel = await this.bonsaiRepository.exist({ where: { id } });
+    if (!bonsaiToDel) {
+      throw new NotFoundException(BONSAI_NOT_FOUND);
+    }
+    await this.bonsaiRepository.delete(id);
   }
 }

ec9178aa94b09cb527b69ab6624060630e5f7af4
Yauhen.kalabukhin <yauhen.kalabukhin@gmail.com>
2023-11-01 16:39:14 +0300

    feat: added whitelist property to validation pipe

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true,

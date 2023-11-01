3d40d72ce66dd6c3d1c01fb307e30efd099717bc
Yauhen.kalabukhin <yauhen.kalabukhin@gmail.com>
2023-11-01 10:06:08 +0300

    feat: implemented controller for get/edit/deletet bonsai by id methods of bonsai service

--- src/bonsai/bonsai.controller.ts
+++ src/bonsai/bonsai.controller.ts
@@ -7,21 +7,26 @@ import {
   HttpStatus,
   NotFoundException,
   Param,
+  Patch,
   Post,
   UseGuards,
 } from '@nestjs/common';
 import { AtPublic } from '../common/decorators/atPublic.decorator.js';
+import { RtPublic } from '../common/decorators/rtPublic.decorator.js';
 import { AtGuard } from '../common/guards/at.guard.js';
+import { RtGuard } from '../common/guards/rt.guard.js';
 import { BonsaiService } from './bonsai.service.js';
 import { BONSAI_NOT_FOUND } from './consts/bonsai.constants.js';
 import { CreateBonsaiDto } from './dto/CreateBonsai.dto.js';
+import { EditBonsaiDto } from './dto/EditBonsai.dto.js';
 
 @Controller('bonsai')
-@UseGuards(AtGuard)
+@UseGuards(AtGuard, RtGuard)
 export class BonsaiController {
   constructor(private readonly bonsaiService: BonsaiService) {}
 
   @AtPublic()
+  @RtPublic()
   @Get()
   @HttpCode(HttpStatus.OK)
   async getAllBonsai() {
@@ -33,25 +38,31 @@ export class BonsaiController {
   }
 
   @AtPublic()
+  @RtPublic()
   @Get(':id')
   @HttpCode(HttpStatus.OK)
   async getBonsaiById(@Param('id') id: string) {
-    const bonsai = await this.bonsaiService.getBonsaiById(+id);
-    if (!bonsai) {
-      throw new NotFoundException(BONSAI_NOT_FOUND);
-    }
-    return bonsai;
+    return await this.bonsaiService.getBonsaiById(+id);
   }
 
   @Post('create')
   @HttpCode(HttpStatus.CREATED)
   async createBonsai(@Body() createBonsaiDto: CreateBonsaiDto) {
-    this.bonsaiService.createBonsai(createBonsaiDto);
+    return await this.bonsaiService.createBonsai(createBonsaiDto);
   }
 
   @Delete('delete/:id')
-  @HttpCode(HttpStatus.NO_CONTENT)
+  @HttpCode(HttpStatus.OK)
   async deleteById(@Param('id') id: string) {
-    await this.bonsaiService.deleteById(+id);
+    return await this.bonsaiService.deleteById(+id);
+  }
+
+  @Patch('edit/:id')
+  @HttpCode(HttpStatus.OK)
+  async editById(
+    @Param('id') id: string,
+    @Body() editBonsaiDto: EditBonsaiDto,
+  ) {
+    return await this.bonsaiService.editById(+id, editBonsaiDto);
   }
 }
